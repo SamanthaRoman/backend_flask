33f6035418db8af42aa14df27d51632225ef1760
Samantha Roman <samantharaeroman@gmail.com>
2023-10-18 22:46:37 -0400

    assignment 3

---
+++ config.py
@@ -0,0 +1,7 @@
+# below is a dictionary 
+me = {
+    "first_name": "Samantha",
+    "last_name": "Roman",
+    "email": "samantharaeroman@gmail.com",
+    "github_url": "https://github.com/SamanthaRoman"
+}
--- server.py
+++ server.py
@@ -1,3 +1,48 @@
-# test change 
+from flask import Flask
+# opposit as java we say from then import. 
+from config import me 
+# this is how you import other files in your project
+import json
 
-print("Server")
+
+# flask is a class thats why it is capital F
+# this is how we use classes
+
+app = Flask(__name__)
+
+
+# the root page has no text after the slash
+@app.get("/")
+def index():
+    return "Hello from Flask"
+
+@app.get("/test")
+def anything():
+    return "This is another page"
+
+
+
+
+#####################################################
+###############        API        ###################
+#####################################################
+
+@app.get("/api/version")
+def version():
+    v = {
+        "version": "1.0.0",
+        "name": "Quartz"
+    }
+    return json.dumps(v)
+# dumps variable converts everyting into json
+
+
+# get /api/about 
+# return the me dictionary as json 
+
+@app.get("/api/about")
+def about():
+    return json.dumps(me)
+
+# use app.run to run it and use the script in the terminal command: python3 filename
+app.run(debug=True)
